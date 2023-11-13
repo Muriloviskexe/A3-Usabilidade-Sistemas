ca85d4f5cb157b835fc873ec8693c92ae450e12a
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-11-13 19:26:22 -0300

    .

--- Back-end/README.md
+++
@@ -1,1 +0,0 @@
-# BackendA3
--- Front-end/src/App.vue
+++ Front-end/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white h-200" id="all">
+  <div class="bg-white h-300" id="all">
     <div class="flex justify-center" id="header">
         <h2 class="text-center p-4 text-5xl font-light font-mono">
             {{ title }}
@@ -11,76 +11,202 @@
       </div>
 
       <div class="absolute top-2 left-6" id="bar">
-        <button v-on:click="openBar()"><i class="fa-solid fa-bars"></i></button>
-      </div>
-    </div>
-    <div class="h-screen w-300px bg-slate-900 fixed -left-100 top-0 z-10 flex justify-end text-white" id="sideBar">
-      <div class="flex gap-3 text-white text-xl absolute top-10 left-10" id="btn-users">
-        <button><i class="fa-regular fa-user"></i></button>
-        <button><i class="fa-regular fa-bell"></i></button>
+        <button @click="show = !show"><i class="fa-solid fa-bars"></i></button>
       </div>
 
-      <div class="absolute top-40 left-10">
-        <h5>Notas</h5>
-        <h5>Tutorial</h5>
-        <h5>Semanas</h5>
-        <h5>Criadores</h5>
-        <h5>Sair</h5>
-      </div>
+        <Transition>
+            <div v-if="show" class="h-screen w-300px bg-slate-900 fixed left-0 top-0 z-10 flex justify-end text-white"><div class="flex gap-3 text-white text-xl absolute top-10 left-10">
+              <button><i class="fa-regular fa-user"></i></button>
+              <button><i class="fa-regular fa-bell"></i></button>
+            </div>
 
-      <div>
-        <button class="relative top-2 right-4 text-white transition ease-in duration-200" v-on:click="closeBar()"><i class="fa-solid fa-xmark"></i></button>
-      </div>
+            <div class="absolute top-40 left-10 flex-col text-2xl grid justify-items-start gap-2">
+              <button class="hover:scale-110">Notas</button>
+              <button class="hover:scale-110">Tutorial</button>
+              <button class="hover:scale-110">Semanas</button>
+              <button class="hover:scale-110">Criadores</button>
+              <button class="hover:scale-110">Sair</button>
+            </div>
+
+            <div>
+            <button class="relative top-2 right-4 text-white" @click="show = !show"><i class="fa-solid fa-xmark"></i></button>
+            </div>
+          </div>
+        </Transition>
       
     </div>
 
-    <div class="grid grid-cols-3">
-      <div v-for="item in cards">
-        <Card :item="item"/>
+    <div class="conteiner grid grid-cols-3 gap-2 justify-center">
+    
+        <div class="cartao border border-black m-4 rounded-xl text-white">
+          <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+            <h2 class="text-2xl">
+              Segunda-Feira
+            </h2>
+
+            <button @click="adcTarefa">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+          </div>
+
+          <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+            <input type="text" class="border" v-model="caixa_tarefa"/>
+            <p>Tarefas:</p>
+
+            <div v-for="p in tarefas">
+              <div class=" p-1 font-mono text-lg m-1">
+                {{ p }}
+              </div>
+          </div>
+
+          </div>
+        </div>
+      
+
+        <div class="cartao border border-black m-4 rounded-xl text-white">
+          <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+            <h2 class="text-2xl">
+              Terça-Feira
+            </h2>
+            <button>
+              <i class="fa-solid fa-plus"></i>
+            </button>
+          </div>
+
+          <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+            <p class="textoTarefa">Tarefas:</p>
+          </div>
+        </div>
+      
+      
+        <div class="cartao border border-black m-4 rounded-xl text-white">
+          <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+            <h2 class="text-2xl">
+              Quarta-Feira
+            </h2>
+            <button>
+              <i class="fa-solid fa-plus"></i>
+            </button>
+          </div>
+          
+          <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+            <p class="textoTarefa">Tarefas:</p>
+          </div>
+        </div>
+      
+
+      <div class="cartao border border-black m-4 rounded-xl text-white">
+        <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+          <h2 class="text-2xl">
+            Quinta-Feira
+          </h2>
+          <button>
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+
+
+        <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+          <p class="textoTarefa">Tarefas:</p>
+        </div>
+      </div>
+
+      <div class="cartao border border-black m-4 rounded-xl text-white">
+        <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+          <h2 class="text-2xl">
+            Sexta-Feira
+          </h2>
+          <button>
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+
+
+        <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+          <p class="textoTarefa">Tarefas:</p>
+        </div>
+      </div>
+
+      <div class="cartao border border-black m-4 rounded-xl text-white">
+        <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+          <h2 class="text-2xl">
+            Sábado
+          </h2>
+          <button>
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+
+
+        <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+          <p class="textoTarefa">Tarefas:</p>
+        </div>
+      </div>
+
+
+      <div class="cartao border border-black m-4 rounded-xl text-white">
+        <div class="p-2 m-1 rounded-xl bg-slate-900 text-white text-center flex justify-around font-EDU">
+          <h2 class="text-2xl">
+            Domingo
+          </h2>
+          <button>
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+
+
+        <div class="p-3 m-1 text-black font-bold font-mono text-lg">
+          <p>Tarefas:</p>
+        </div>
+
       </div>
     </div>
+    
   </div>
 </template>
 
-<script>
-import Card from "./components/Card/Card.vue"
-
+<script >
 export default {
-  components: { Card },
   data() {
     return {
-      title: "Planner Online",
-      cards: [
-        { title: 'Segunda-feira', body: 'Tarefas:' },
-        { title: 'Terça-feira', body: 'Tarefas:' },
-        { title: 'Quarta-feira', body: 'Tarefas:' },
-        { title: 'Quinta-feira', body: 'Tarefas:' },
-        { title: 'Sexta-feira', body: 'Tarefas:' },
-        { title: 'Sabado', body: 'Tarefas:' },
-        { title: 'Domingo', body: 'Tarefas:' },
+      tarefas: [],
 
-  
-      ]
+      caixa_tarefa: '',
+
+      show:false,
+
+      title: "Planner Online",
     }
   },
   methods: {
     darkMode: function(){
-      document.getElementById('header').classList.add('bg-slate-900')
+      document.getElementById('header').classList.add('bg-slate-950')
       document.getElementById('header').classList.add('text-white')
+
       document.getElementById('all').classList.remove('bg-white')
-      document.getElementById('all').classList.add('bg-slate-600')
-      document.getElementById('card').classList.add('text-white')
-      document.getElementById('card').classList.remove('text-black')
+      document.getElementById('all').classList.add('bg-slate-900')
+    
+      document.getElementById('body').classList.remove('text-black')
+      document.getElementById('body').classList.add('text-white')
+
+      document.getElementsByName('cartao').classList.remove('border-black')
+      document.getElementsByName('cartao').classList.add('border-white')
+
 
 
     },
     lightMode: function(){
       document.getElementById('header').classList.remove('bg-slate-900')
       document.getElementById('header').classList.remove('text-white')
+
       document.getElementById('all').classList.add('text-slate-900')
       document.getElementById('all').classList.add('bg-white')
-      document.getElementById('card').classList.add('text-black')
-      document.getElementById('card').classList.remove('text-white')
+  
+      document.getElementById('body').classList.add('text-black')
+      document.getElementById('body').classList.remove('text-white')
+
+      document.getElementsByName('cartao').classList.add('border-black')
+      document.getElementsByName('cartao').classList.remove('border-white')
 
     },
     openBar: function(){
@@ -92,6 +218,14 @@ export default {
       document.getElementById('sideBar').classList.add('-left-100')
       document.getElementById('sideBar').classList.remove('-left-0')
     },
+
+    adcTarefa: function(){
+      let temp = this.caixa_tarefa.charAt(0).toUpperCase() + this.caixa_tarefa.slice(1)
+      this.tarefas.push(temp)
+      this.caixa_tarefa = ''
+    }
+
+    
   }
 }
 
@@ -99,4 +233,24 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner&family=Indie+Flower&display=swap');
+
+.v-enter-active{
+  transition: opacity 200ms ease-in-out;
+}
+
+.v-leave-active{
+  transition: all 300ms cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(-40px);  
+  opacity: 0;
+}
+
+.darkTheme{
+  background-color: #2d2f31;
+}
+
+
 </style>
