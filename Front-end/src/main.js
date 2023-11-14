1472719c05407e164fe1538857f78f05bbbaea7e
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-11-13 21:18:12 -0300

    fazendo o js

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -1,35 +1,8 @@
-export default {
-  data() {
-    return {
-      tarefas: [],
-
-      caixa_tarefa: '',
-
-      show:false,
-
-      title: "Planner Online",
-    }
-  },
-  methods: {
-    darkMode: function(){
-      document.getElementById('header').classList.add('bg-slate-950')
-      document.getElementById('header').classList.add('text-white')
-
-      document.getElementById('all').classList.remove('bg-white')
-      document.getElementById('all').classList.add('bg-slate-900')
-
-      document.getElementById('body').classList.remove('text-black')
-      document.getElementById('body').classList.add('text-white')
-
-      document.getElementsByName('cartao').classList.remove('border-black')
-      document.getElementsByName('cartao').classList.add('border-white')
-
-      document.getElementById('botaoNoite').classList.add('esconder')
-      document.getElementById('botaoDia').classList.remove('esconder')
+function DarkMode(){
 
 
-    },
-    lightMode: function(){
+};
+lightMode: function(){
       document.getElementById('header').classList.remove('bg-slate-900')
       document.getElementById('header').classList.remove('text-white')
 
@@ -43,22 +16,17 @@ export default {
       document.getElementsByName('cartao').classList.remove('border-white')
 
     },
-    openBar: function(){
+ openBar: function(){
       document.getElementById('sideBar').classList.remove('-left-100')
       document.getElementById('sideBar').classList.add('-left-0')
 
     },
     closeBar: function() {
       document.getElementById('sideBar').classList.add('-left-100')
-      document.getElementById('sideBar').classList.remove('-left-0')
-    },
+      document.getElementById('sideBar').classList.remove('-left-0')},
 
-    adcTarefa: function(){
+adcTarefa: function(){
       let temp = this.caixa_tarefa.charAt(0).toUpperCase() + this.caixa_tarefa.slice(1)
       this.tarefas.push(temp)
       this.caixa_tarefa = ''
-    }
-
-    
-  }
 }
