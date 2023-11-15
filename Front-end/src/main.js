f840bc4fc20125123480abc07e351c3528121766
Muriloviskexe <murilogccontato@gmail.com>
2023-11-15 16:58:16 -0300

    Atualização 15/11
    Adição de tarefas apenas no primeiro dia,  modo escuro e claro feito (apenas atualizar tudo), ainda sem sidebar, sem post

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -8,19 +8,14 @@ document.querySelector('#titulo').classList.remove('tituloLight')
 document.querySelector('#cabeca').classList.add('cabecaDark')
 document.querySelector('.DarkModeButtons').classList.add('cabecaDark')
 document.querySelector('html').classList.add('darkPage')
-
 document.querySelector('#conteiner-card').classList.add('conteiner-cardDark')
 document.querySelector('#conteiner-card').classList.remove('conteiner-card')
-
 document.querySelector('#header-card').classList.add('header-cardDark')
 document.querySelector('#header-card').classList.remove('header-card')
+document.querySelector('.text-tarefa').classList.remove('text-light')
+document.querySelector('.text-tarefa').classList.add('text-dark')
 
-
-
-
-
-
-};
+}
 
 function lightMode(){
 
@@ -30,11 +25,27 @@ document.querySelector('#titulo').classList.remove('tituloDark')
 document.querySelector('#titulo').classList.add('tituloLight')
 document.querySelector('#cabeca').classList.remove('cabecaDark')
 document.querySelector('html').classList.remove('darkPage')
-
 document.querySelector('#conteiner-card').classList.remove('conteiner-cardDark')
 document.querySelector('#conteiner-card').classList.add('conteiner-card')
-
 document.querySelector('#header-card').classList.remove('header-cardDark')
 document.querySelector('#header-card').classList.add('header-card')
+document.querySelector('.text-tarefa').classList.add('text-light')
+document.querySelector('.text-tarefa').classList.remove('text-dark')
 
-};
+}
+
+function adcTarefa(){
+    var tarefa = document.querySelector('#input-segunda');
+     const value = tarefa.value.trim(); // Remova espaços em branco antes e depois do valor
+
+     if (value !== '') {
+         const segunda = document.getElementById('segunda');
+         const novaTarefa = document.createElement('li');
+         novaTarefa.textContent = value;
+         segunda.appendChild(novaTarefa);
+
+         tarefa.value = ''; // Limpa o campo de entrada após adicionar a tarefa
+     } else {
+         alert('Por favor, insira uma tarefa válida.');
+     }
+}
