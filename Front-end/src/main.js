c1ec660bc4e987564c1ee1b6c2fc144c8b4f6a5b
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-11-14 21:37:39 -0300

    concluindo integrações do modo escuro

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -1,32 +1,40 @@
-function DarkMode(){
+
+function darkMode(){
+
+document.getElementById('dark').classList.add('hide')
+document.getElementById('light').classList.remove('hide')
+document.querySelector('#titulo').classList.add('tituloDark')
+document.querySelector('#titulo').classList.remove('tituloLight')
+document.querySelector('#cabeca').classList.add('cabecaDark')
+document.querySelector('.DarkModeButtons').classList.add('cabecaDark')
+document.querySelector('html').classList.add('darkPage')
+
+document.querySelector('#conteiner-card').classList.add('conteiner-cardDark')
+document.querySelector('#conteiner-card').classList.remove('conteiner-card')
+
+document.querySelector('#header-card').classList.add('header-cardDark')
+document.querySelector('#header-card').classList.remove('header-card')
+
+
+
+
 
 
 };
-lightMode: function(){
-      document.getElementById('header').classList.remove('bg-slate-900')
-      document.getElementById('header').classList.remove('text-white')
 
-      document.getElementById('all').classList.add('text-slate-900')
-      document.getElementById('all').classList.add('bg-white')
-  
-      document.getElementById('body').classList.add('text-black')
-      document.getElementById('body').classList.remove('text-white')
+function lightMode(){
 
-      document.getElementsByName('cartao').classList.add('border-black')
-      document.getElementsByName('cartao').classList.remove('border-white')
+document.getElementById('dark').classList.remove('hide')
+document.getElementById('light').classList.add('hide')
+document.querySelector('#titulo').classList.remove('tituloDark')
+document.querySelector('#titulo').classList.add('tituloLight')
+document.querySelector('#cabeca').classList.remove('cabecaDark')
+document.querySelector('html').classList.remove('darkPage')
 
-    },
- openBar: function(){
-      document.getElementById('sideBar').classList.remove('-left-100')
-      document.getElementById('sideBar').classList.add('-left-0')
+document.querySelector('#conteiner-card').classList.remove('conteiner-cardDark')
+document.querySelector('#conteiner-card').classList.add('conteiner-card')
 
-    },
-    closeBar: function() {
-      document.getElementById('sideBar').classList.add('-left-100')
-      document.getElementById('sideBar').classList.remove('-left-0')},
+document.querySelector('#header-card').classList.remove('header-cardDark')
+document.querySelector('#header-card').classList.add('header-card')
 
-adcTarefa: function(){
-      let temp = this.caixa_tarefa.charAt(0).toUpperCase() + this.caixa_tarefa.slice(1)
-      this.tarefas.push(temp)
-      this.caixa_tarefa = ''
-}
+};
